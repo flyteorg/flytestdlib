4208ee65471dede302e55c6dbdce6fc08c65fed1
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-10-24 03:15:43 -0700

    Implement config generation cmd

--- config/config_cmd.go
+++ config/config_cmd.go
@@ -2,8 +2,13 @@ package config
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -14,6 +19,7 @@ const (
 	StrictModeFlag  = "strict"
 	CommandValidate = "validate"
 	CommandDiscover = "discover"
+	CommandGenerate = "generate"
 )
 
 type AccessorProvider func(options Options) Accessor
@@ -28,11 +34,14 @@ func NewConfigCommand(accessorProvider AccessorProvider) *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:       "config",
 		Short:     "Runs various config commands, look at the help of this command to get a list of available commands..",
-		ValidArgs: []string{CommandValidate, CommandDiscover},
+		ValidArgs: []string{CommandValidate, CommandDiscover, CommandGenerate},
 	}
 
+	rootCmd.PersistentFlags().BoolVar(&opts.StrictMode, StrictModeFlag, false, `Validates that all keys in loaded config
+map to already registered sections.`)
+
 	validateCmd := &cobra.Command{
-		Use:   "validate",
+		Use:   CommandValidate,
 		Short: "Validates the loaded config.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return validate(accessorProvider(opts), cmd)
@@ -40,12 +49,25 @@ func NewConfigCommand(accessorProvider AccessorProvider) *cobra.Command {
 	}
 
 	discoverCmd := &cobra.Command{
-		Use:   "discover",
+		Use:   CommandDiscover,
 		Short: "Searches for a config in one of the default search paths.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return validate(accessorProvider(opts), cmd)
 		},
 	}
+
+	var outputPath string
+
+	generateCmd := &cobra.Command{
+		Use:   CommandGenerate,
+		Short: "Generates a single config file by compiling all default values and optionally provided config files.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return generate(accessorProvider(opts), cmd, outputPath)
+		},
+	}
+
+	generateCmd.Flags().StringVarP(&outputPath, "output-path", "o", "", "Where to write"+
+		" the generated config")
 
 	// Configure Root Command
 	rootCmd.PersistentFlags().StringArrayVar(&opts.SearchPaths, PathFlag, []string{}, `Passes the config file to load.
@@ -53,10 +75,7 @@ If empty, it'll first search for the config file path then, if found, will load 
 
 	rootCmd.AddCommand(validateCmd)
 	rootCmd.AddCommand(discoverCmd)
-
-	// Configure Validate Command
-	validateCmd.Flags().BoolVar(&opts.StrictMode, StrictModeFlag, false, `Validates that all keys in loaded config
-map to already registered sections.`)
+	rootCmd.AddCommand(generateCmd)
 
 	return rootCmd
 }
@@ -73,6 +92,76 @@ func redirectStdOut() (old, new *os.File) {
 	os.Stdout = new
 
 	return
+}
+
+func generate(accessor Accessor, p printer, outputPath string) error {
+	// Redirect stdout
+	old, n := redirectStdOut()
+	defer func() {
+		err := n.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	defer func() { os.Stdout = old }()
+
+	err := accessor.UpdateConfig(context.Background())
+	if err != nil {
+		red := color.New(color.FgRed).SprintFunc()
+		p.Println(red("Failed to validate config file."))
+		return err
+	}
+
+	printInfo(p, accessor)
+	green := color.New(color.FgGreen).SprintFunc()
+	p.Println(green("Validated config file successfully."))
+
+	p.Printf("Generating config at: %v\r\n", outputPath)
+	rootSection := GetRootSection()
+	m, err := createMap(rootSection)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := yaml.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	if err = ioutil.WriteFile(filepath.Join(outputPath, "config_all.yaml"), bytes, os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createMap(section Section) (res map[string]interface{}, err error) {
+	res = map[string]interface{}{}
+	if rootConfig := section.GetConfig(); rootConfig != nil {
+		res, err = toMap(rootConfig)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for subName, sub := range section.GetSections() {
+		res[subName], err = createMap(sub)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return res, nil
+}
+
+func toMap(config Config) (map[string]interface{}, error) {
+	bytes, err := json.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	res := map[string]interface{}{}
+	return res, json.Unmarshal(bytes, &res)
 }
 
 func validate(accessor Accessor, p printer) error {
--- config/config_cmd_test.go
+++ config/config_cmd_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"flag"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/spf13/cobra"
@@ -54,11 +56,91 @@ func TestNewConfigCommand(t *testing.T) {
 	cmd := NewConfigCommand(newMockAccessor)
 	assert.NotNil(t, cmd)
 
-	output, err := executeCommandC(cmd, CommandDiscover)
-	assert.NoError(t, err)
-	assert.Contains(t, output, "test")
+	t.Run("Discover", func(t *testing.T) {
+		output, err := executeCommandC(cmd, CommandDiscover)
+		assert.NoError(t, err)
+		assert.Contains(t, output, "test")
+	})
 
-	output, err = executeCommandC(cmd, CommandValidate)
+	t.Run("Validate", func(t *testing.T) {
+		output, err := executeCommandC(cmd, CommandValidate)
+		assert.NoError(t, err)
+		assert.Contains(t, output, "test")
+	})
+
+	t.Run("Generate", func(t *testing.T) {
+		path, err := ioutil.TempDir(os.TempDir(), "generate-test")
+		assert.NoError(t, err)
+
+		output, err := executeCommandC(cmd, CommandGenerate, "-o", path)
+		assert.NoError(t, err)
+		assert.Contains(t, output, "test")
+	})
+}
+
+type TestType struct {
+	String  string    `json:"string,omitempty"`
+	Int     int       `json:"i,omitempty"`
+	Recurse *TestType `json:"TestType,omitempty"`
+}
+
+func TestCreateMap(t *testing.T) {
+
+	s := NewRootSection()
+	assert.NoError(t, s.SetConfig(&TestType{
+		String: "Root",
+		Int:    0,
+	}))
+
+	_, err := s.RegisterSection("sub1", &TestType{
+		String: "sub1",
+		Int:    1,
+	})
+
 	assert.NoError(t, err)
-	assert.Contains(t, output, "test")
+
+	_, err = s.RegisterSection("sub2", &TestType{
+		String: "sub2",
+		Int:    1,
+		Recurse: &TestType{
+			String: "subsub",
+			Int:    2,
+		},
+	})
+
+	expected := map[string]interface{}{
+		"string": "Root",
+		"sub1": map[string]interface{}{
+			"string": "sub1",
+			"i":      float64(1),
+		},
+		"sub2": map[string]interface{}{
+			"string": "sub2",
+			"i":      float64(1),
+			"TestType": map[string]interface{}{
+				"string": "subsub",
+				"i":      float64(2),
+			},
+		},
+	}
+
+	m, err := createMap(s)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, m)
+}
+
+func Test_toMap(t *testing.T) {
+	input := TestType{
+		String: "something",
+		Int:    4,
+	}
+
+	expected := map[string]interface{}{
+		"string": "something",
+		"i":      float64(4),
+	}
+
+	m, err := toMap(input)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, m)
 }
