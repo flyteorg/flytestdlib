c16fc474fa8aee67532cfa1b2d173997210343bd
Haytham Abuelfutuh <haytham@afutuh.com>
2021-05-19 15:43:34 -0700

    Increase patch coverage
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- config/viper/viper_test.go
+++ config/viper/viper_test.go
@@ -25,9 +25,29 @@ func Test_stringToByteArray(t *testing.T) {
 		assert.Equal(t, []byte(input[0]), res)
 	})
 
+	t.Run("Expected types - invalid encoding", func(t *testing.T) {
+		input := []string{"hello world"}
+		_, err := stringToByteArray(reflect.TypeOf(input), reflect.TypeOf([]byte{}), []string{"invalid base64"})
+		assert.Error(t, err)
+	})
+
+	t.Run("Expected types - empty array string", func(t *testing.T) {
+		input := []string{"hello world"}
+		res, err := stringToByteArray(reflect.TypeOf(input), reflect.TypeOf([]byte{}), []string{})
+		assert.NoError(t, err)
+		assert.Equal(t, []string{}, res)
+	})
+
 	t.Run("Unexpected types", func(t *testing.T) {
 		input := 5
 		res, err := stringToByteArray(reflect.TypeOf(input), reflect.TypeOf([]byte{}), input)
+		assert.NoError(t, err)
+		assert.NotEqual(t, []byte("hello"), res)
+	})
+
+	t.Run("Unexpected types", func(t *testing.T) {
+		input := 5
+		res, err := stringToByteArray(reflect.TypeOf(input), reflect.TypeOf(""), input)
 		assert.NoError(t, err)
 		assert.NotEqual(t, []byte("hello"), res)
 	})
