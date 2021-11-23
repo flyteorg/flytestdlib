97d84b2e857c72e51cb632e9db582be84c44c77f
Babis Kiosidis <ckiosidis@gmail.com>
2021-11-23 17:26:56 +0200

    test failed to marshal
    Signed-off-by: Babis Kiosidis <ckiosidis@gmail.com>

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -614,3 +614,29 @@ func Test_gcpFormatter(t *testing.T) {
 		})
 	}
 }
+
+func Test_gcpFormatterFails(t *testing.T) {
+	tests := []struct {
+		name   string
+		entry  *logrus.Entry
+		errMsg string
+	}{
+		{"test", &logrus.Entry{
+			Logger:  nil,
+			Data:    map[string]interface{}{"src": make(chan int)},
+			Time:    time.Date(2000, 01, 01, 01, 01, 000, 000, time.UTC),
+			Level:   1,
+			Caller:  nil,
+			Message: "some-message",
+			Buffer:  nil,
+			Context: nil,
+		}, "Failed to marshal fields to JSON, json: unsupported type: chan int"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			formatter := GcpFormatter{}
+			_, err := formatter.Format(tt.entry)
+			assert.Equal(t, tt.errMsg, err.Error())
+		})
+	}
+}
