38ab7183a56213537f17bb75388a35bf97e45bd5
Kevin Su <pingsutw@apache.org>
2022-03-29 17:58:42 +0800

    More tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- database/config_test.go
+++ database/config_test.go
@@ -26,6 +26,7 @@ func TestParseDatabaseConfig(t *testing.T) {
 	assert.Equal(t, 10, GetConfig().MaxIdleConnections)
 	assert.Equal(t, config.Duration{Duration: 3600000000000}, GetConfig().ConnMaxLifeTime)
 
+	assert.Equal(t, false, GetConfig().Postgres.IsEmpty())
 	assert.Equal(t, 5432, GetConfig().Postgres.Port)
 	assert.Equal(t, "postgres", GetConfig().Postgres.User)
 	assert.Equal(t, "postgres", GetConfig().Postgres.Host)
@@ -35,5 +36,6 @@ func TestParseDatabaseConfig(t *testing.T) {
 	assert.Equal(t, "/etc/secret", GetConfig().Postgres.PasswordPath)
 	assert.Equal(t, true, GetConfig().Postgres.Debug)
 
+	assert.Equal(t, false, GetConfig().SQLite.IsEmpty())
 	assert.Equal(t, "admin.db", GetConfig().SQLite.File)
 }
